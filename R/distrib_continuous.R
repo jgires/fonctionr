d9082949a81e8276f78463fdbe2db6310fb93b6c
Joel Gires <joel.gires@vivalis.brussels>
2024-03-21 23:34:17 +0100

    Première version fonctionne de distrib_continuous() !

--- R/distrib_continuous.R
+++ R/distrib_continuous.R
@@ -45,6 +45,8 @@ distrib_continuous <- function(data,
                           filter_exp = NULL,
                           ...,
                           na.rm.facet = T,
+                          limits = NULL,
+                          bw = 1,
                           show_ci = T,
                           show_n = FALSE,
                           show_value = TRUE,
@@ -52,7 +54,8 @@ distrib_continuous <- function(data,
                           digits = 0,
                           unit = "",
                           dec = ",",
-                          pal = NULL,
+                          pal = c("#c94d33", "#ffe3bf"),
+                          border = NA,
                           font ="Roboto",
                           title = NULL,
                           subtitle = NULL,
@@ -61,7 +64,7 @@ distrib_continuous <- function(data,
                           caption = NULL,
                           export_path = NULL) {
 
-  # On convertit d'abord en objet srvyr
+  # On convertit d'abord data en objet srvyr
   data_W <- convert_to_srvyr(data, ...)
 
   # On recalcule quanti_exp dans une variable unique si c'est une expression à la base => nécessaire pour calculer la densité
@@ -70,16 +73,18 @@ distrib_continuous <- function(data,
       quanti_exp_flattened = {{ quanti_exp }}
     )
 
+  # On identifie la variable de pondération inclue dans ... (pour srvyr) pour la passer aussi à la densité
   var_weights <- as.character(substitute(...()))[names(as.list(substitute(...()))) %in% c("weights", "weight")]
 
-  # Estimated probability density
+  # On estime la densité de la variable quanti
   estDensity <- density(data_W$variables[["quanti_exp_flattened"]],
-    adjust = 1,
-    n = 4096,
-    weights = data_W$variables[[var_weights]],
+    adjust = bw,
+    subdensity = T,
+    weights = data_W$variables[[var_weights]], # On introduit la variable de pondération identifiée dans var_weights
     na.rm = T
   )
 
+  # On calcule les quantiles avec survey et on les stocke dans un vecteur
   estQuant_W <- as.numeric(svyquantile(~quanti_exp_flattened,
     design = data_W,
     quantiles = quantiles,
@@ -87,39 +92,97 @@ distrib_continuous <- function(data,
     na.rm = T
   )[[1]])
 
+  # On crée un data.frame, et on détermine à quelle quantile x et y appartiennent en croisant avec le vecteur de quantiles
   df_dens <- data.frame(
     x = estDensity$x,
     y = estDensity$y / sum(estDensity$y),
-    quantFct = as.factor(findInterval(estDensity$x, estQuant_W) + 1)
+    quantFct = findInterval(estDensity$x, estQuant_W) + 1
   )
 
   # sum(df_dens$y)
 
-  ggplot(
+  # Il faut qu'il y ait dans l'estimation de la densité les valeurs EXACTES des quantiles, pour que le ggplot coupe exactement aux quantiles !
+  # => Je les ajoute avec add_case, en estimant y avec approx()
+  # Je le fais dans une boucle, pour avoir tous les quantiles.
+  # SOLUTION TROUVéE ICI : https://stackoverflow.com/questions/74560448/how-fill-geom-ribbon-with-different-colour-in-r
+  for(i in seq_along(quantiles)){
+    df_dens <- df_dens |>
+      add_case(
+        x = estQuant_W[i],
+        y = approx(df_dens$x, df_dens$y, xout = estQuant_W[i])$y,
+        quantFct = i
+      ) %>%
+      add_case(
+        x = estQuant_W[i],
+        y = approx(df_dens$x, df_dens$y, xout = estQuant_W[i])$y,
+        quantFct = i+1
+      )
+  }
+
+  # Je transforme l'appartenance au quantile en facteur, pour le ggplot
+  df_dens <- df_dens %>%
+    mutate(quantFct = as.factor(quantFct))
+
+  # La palette divergente
+  if(length(estQuant_W) %% 2 == 0){
+    palette <- colorRampPalette(pal)((length(estQuant_W)/2)+1)
+    palette <- c(palette, rev(palette)[-1])
+  }
+  if(length(estQuant_W) %% 2 == 1){
+    palette <- colorRampPalette(pal)(((length(estQuant_W)+1)/2)+1)
+    palette <- c(rev(rev(palette)[-2]), rev(palette)[-1])
+  }
+
+  # Le graphique ggplot
+
+  graph <- ggplot(
     data = df_dens
   ) +
     geom_line(
       aes(
         x = x,
         y = y
-      )
+      ),
+      color = border
     ) +
-    geom_area(
+    geom_ribbon(
       aes(
-        x = x, y = y, fill = quantFct
+        x = x, ymin = 0, ymax = y, fill = quantFct
       ),
       alpha = 1
     ) +
     scale_x_continuous(
-      breaks = estQuant_W
+      breaks = estQuant_W,
+      labels = label_dollar(
+        accuracy = 1,
+        prefix = "",
+        big.mark = ".",
+        decimal.mark = ","),
+      limits = limits
+    ) +
+    scale_y_continuous(
+      expand = expansion(mult = c(0, 0.05))
     ) +
     scale_fill_manual(
       name = "",
-      values = c("green", "yellow", "orange", "red")
+      values = palette
     ) +
     labs(title = "Probability Density") +
     xlab("Numeric variable") +
     ylab("Estimated Density") +
-    theme_minimal()
+    theme_minimal() +
+    theme(
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.x = element_blank(),
+      axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+      legend.position = 'none'
+    )
+
+  # On crée l'objet final
+  res <- list()
+  res$dens <- df_dens
+  res$graph <- graph
+
+  return(res)
 
 }
